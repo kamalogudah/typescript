8541ec4dd349f71da78452a6f048e9466022bcc8
kamalogudah <mcpaul2058@gmail.com>
2020-04-15 20:05:08 +0300

    Typescript starting

---
+++ features/types.ts
@@ -0,0 +1,16 @@
+const today = new Date();
+
+today.getTime();
+
+const person = {
+  age: 20,
+};
+
+person.age;
+
+class Color {}
+
+const red = new Color();
+
+// Type annotation => code we add to tell typescript what type of value a varible will refer to
+// Type inference => Typescript tries to figure out what type of value a variable referes to
