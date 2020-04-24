32e342016a38e8848e730d3b1a5b55348a0b8bb0
kamalogudah <mcpaul2058@gmail.com>
2020-04-24 12:09:36 +0300

    Duplicate Code

--- maps/dist/src.f10117fe.js
+++ maps/dist/src.f10117fe.js
@@ -85185,7 +85185,15 @@ function () {
     });
   };
 
-  CustomMap.prototype.addCompanyMarker = function (company) {};
+  CustomMap.prototype.addCompanyMarker = function (company) {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: company.location.lat,
+        lng: company.location.lon
+      }
+    });
+  };
 
   return CustomMap;
 }();
@@ -85208,6 +85216,7 @@ var user = new User_1.User();
 var company = new Company_1.Company();
 var customMap = new CustomMap_1.CustomMap('map');
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
 },{"./User":"src/User.ts","./Company":"src/Company.ts","./CustomMap":"src/CustomMap.ts"}],"../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
