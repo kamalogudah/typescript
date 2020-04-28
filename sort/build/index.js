c138ea7c640dc8d8ce39b6c5e8b94b2435903d17
kamalogudah <mcpaul2058@gmail.com>
2020-04-28 14:20:07 +0300

    Type Guards

--- sort/build/index.js
+++ sort/build/index.js
@@ -11,10 +11,15 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                //if collection is a number
+                if (this.collection instanceof Array) {
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                if (typeof this.collection === 'string') {
                 }
             }
         }
