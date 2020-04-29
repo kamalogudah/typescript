6222f582b1656ecc04d60353d6657abc21fa18f4
kamalogudah <mcpaul2058@gmail.com>
2020-04-29 16:10:44 +0300

    Extracting Key Logic

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,31 +1,4 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    // collection: number[];
-    // constructor(collection: number[]) {
-    //   this.collection = collection;
-    // }
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                //if collection is a number
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                if (typeof this.collection === 'string') {
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
 var sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,31 +1,3 @@
-class Sorter {
-  // collection: number[];
-
-  // constructor(collection: number[]) {
-  //   this.collection = collection;
-  // }
-  constructor(public collection: number[] | string) {}
-
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        //if collection is a number
-        if (this.collection instanceof Array) {
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
-
-        if (typeof this.collection === 'string') {
-        }
-      }
-    }
-  }
-}
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
