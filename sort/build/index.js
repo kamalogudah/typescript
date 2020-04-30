8a0e2f053bb61f955d8362524b446bcf2c869275
kamalogudah <mcpaul2058@gmail.com>
2020-04-30 18:17:40 +0300

    Interface Definition

--- sort/build/index.js
+++ sort/build/index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var sorter_1 = require("./sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([10999, 3, -5, 0, 90, -10]);
+var numbersCollection = new NumbersCollection_1.NumbersCollection([99, 3, -5, 0, 90, -10]);
 var sorter = new sorter_1.Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,7 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
 
-const numbersCollection = new NumbersCollection([10999, 3, -5, 0, 90, -10]);
+const numbersCollection = new NumbersCollection([99, 3, -5, 0, 90, -10]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
