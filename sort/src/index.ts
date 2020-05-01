1cf6da904c194da79eb7a0892f407dce47328d82
kamalogudah <mcpaul2058@gmail.com>
2020-05-01 03:18:30 +0300

    Abstract Classes

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -4,21 +4,20 @@ import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([99, 3, -5, 0, 90, -10]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Pauloguda');
-const stringSorter = new Sorter(charactersCollection);
-stringSorter.sort();
-console.log(charactersCollection.data);
+// const charactersCollection = new CharactersCollection('Pauloguda');
+// const stringSorter = new Sorter(charactersCollection);
+// stringSorter.sort();
+// console.log(charactersCollection.data);
 
-const linkedList = new LinkedList();
-linkedList.add(50);
-linkedList.add(-7);
-linkedList.add(0);
-linkedList.add(5);
-linkedList.add(2);
-const lsSorter = new Sorter(linkedList);
-lsSorter.sort();
-linkedList.print();
+// const linkedList = new LinkedList();
+// linkedList.add(50);
+// linkedList.add(-7);
+// linkedList.add(0);
+// linkedList.add(5);
+// linkedList.add(2);
+// const lsSorter = new Sorter(linkedList);
+// lsSorter.sort();
+// linkedList.print();
