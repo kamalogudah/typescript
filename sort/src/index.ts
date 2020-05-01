c77188548fcd67e1596d8688d953041a16b35867
kamalogudah <mcpaul2058@gmail.com>
2020-05-01 03:35:45 +0300

    User inheritence for other implementations

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -7,17 +7,16 @@ const numbersCollection = new NumbersCollection([99, 3, -5, 0, 90, -10]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Pauloguda');
-// const stringSorter = new Sorter(charactersCollection);
-// stringSorter.sort();
-// console.log(charactersCollection.data);
+const charactersCollection = new CharactersCollection('Pauloguda');
+charactersCollection.sort();
+console.log(charactersCollection.data);
 
-// const linkedList = new LinkedList();
-// linkedList.add(50);
-// linkedList.add(-7);
-// linkedList.add(0);
-// linkedList.add(5);
-// linkedList.add(2);
-// const lsSorter = new Sorter(linkedList);
-// lsSorter.sort();
-// linkedList.print();
+const linkedList = new LinkedList();
+linkedList.add(50);
+linkedList.add(-7);
+linkedList.add(0);
+linkedList.add(5);
+linkedList.add(-1000);
+
+linkedList.sort();
+linkedList.print();
