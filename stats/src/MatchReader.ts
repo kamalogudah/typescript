a69bb3b00fd5444536d278b03e1536bae40b275a
kamalogudah <mcpaul2058@gmail.com>
2020-05-06 09:10:39 +0300

    Extracting Match References

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -1,9 +1,26 @@
+import { dateStringToDate } from '../src/utils';
+
+import { MatchResult } from './MatchResult';
+
+type MatchData = [Date, string, string, number, number, MatchResult, string];
 interface DataReader {
   read(): void;
   data: string[][];
 }
 
 export class MatchReader {
-  constructor(public reader: DataReader){}
+  constructor(public reader: DataReader) { }
+  // .map((row: string[]): MatchData => {
+  //       return [
+  //         dateStringToDate(row[0]),
+  //         row[1],
+  //         row[2],
+  //         parseInt(row[3]),
+  //         parseInt(row[4]),
+  //         // assertion
+  //         row[5] as MatchResult,
+  //         row[6]
+  //       ];
+  //   })
 
 }
