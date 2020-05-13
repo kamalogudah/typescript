c8202d352ce641f1e62efdbdbf925d632dc77f0c
kamalogudah <mcpaul2058@gmail.com>
2020-05-13 13:30:51 +0300

    Refactor to use composition on other parts

--- stats/src/MatchReader.ts
+++ stats/src/MatchReader.ts
@@ -4,12 +4,18 @@ import { MatchResult } from './MatchResult';
 
 import { MatchData } from './MatchData';
 
+import { CsvFileReader } from './CsvFileReader';
+
 interface DataReader {
   read(): void;
   data: string[][];
 }
 
 export class MatchReader {
+  static fromCsv(filename: string): MatchReader {
+    return new MatchReader(new CsvFileReader(filename))
+    
+  }
   matches: MatchData[] = [];
   constructor(public reader: DataReader) { }
   load(): void{
