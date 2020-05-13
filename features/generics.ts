d05cabb4d5fe95f1804668f2dab3d1900b6c4eb9
kamalogudah <mcpaul2058@gmail.com>
2020-05-13 14:03:01 +0300

    Function Generics

--- features/generics.ts
+++ features/generics.ts
@@ -25,4 +25,29 @@ class ArrayOfAnything<T> {
 
 new ArrayOfAnything<string>(['a', 'b', 'c'])
 
-const nums = new ArrayOfAnything([1,2,3,4,5])
+const nums = new ArrayOfAnything([1, 2, 3, 4, 5])
+
+// Generics with Functions
+
+const printStrings = (arr: string[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+   
+}
+ 
+const printNumbers = (arr: number[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+const printAnything = < T extends {} >(arr: T[]): void => {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+  }
+};
+
+printAnything<string>(['a', 'b', 'c']);
+
+printAnything([1,2,3,4]);
