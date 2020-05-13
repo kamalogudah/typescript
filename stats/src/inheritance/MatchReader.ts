10e4c63c03f6ba500c9ec32c9e91127834b440e5
kamalogudah <mcpaul2058@gmail.com>
2020-05-13 10:53:00 +0300

    Building the Reporter

--- stats/src/inheritance/MatchReader.ts
+++ stats/src/inheritance/MatchReader.ts
@@ -1,6 +1,6 @@
 import { CsvFileReader } from './CsvFileReader';
-import { dateStringToDate } from '../src/utils';
-import { MatchResult } from '../src/MatchResult';
+import { dateStringToDate } from '../utils';
+import { MatchResult } from '../MatchResult';
 
 type MatchData = [Date, string, string, number, number, MatchResult, string];
 
