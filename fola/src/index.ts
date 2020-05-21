ff6524377588ceef9d27cbe1fd5b4a57acd1bd9e
kamalogudah <mcpaul2058@gmail.com>
2020-05-21 11:15:49 +0300

    Optional Interface Properties

--- fola/src/index.ts
+++ fola/src/index.ts
@@ -1,6 +1,7 @@
 import { User } from './models/User';
 
 const user = new User({ name: "Paul", age: 20 });
-
+user.set({ name: "Kamal", age: 32 })
+user.set({age: 44})
 console.log(user.get('name'));
 console.log(user.get('age'));
